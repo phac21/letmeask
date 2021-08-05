68ef3d6d70f6fa0b89324f2bb3714ca9a713be0b
Pedro Henrique <pedro.semear@gmail.com>
2021-08-05 00:02:53 -0300

    Aula 4 - Landing

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,88 @@
+//React
+import { useHistory, useParams } from 'react-router-dom';
+
+//Components
+import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+import { Question } from '../components/Question';
+
+//Styles and images
+import '../styles/room.scss';
+
+import logoimg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
+
+//hooks
+import { useRoom } from '../hooks/useRoom';
+// import { useAuth } from '../hooks/useAuth';
+
+//services
+import { database } from '../services/firebase';
+
+type RoomParams = {
+    id: string;
+}
+
+export function AdminRoom(){
+    //const { user } = useAuth();
+    const history = useHistory()
+    const params = useParams<RoomParams>();
+
+    const roomId = params.id
+    const { questions, title } = useRoom(roomId)
+
+    async function handleEndRoom() {
+        await database.ref(`rooms/${roomId}`).update({
+            endedAt: new Date(),
+        })
+
+        history.push('/');
+    }
+
+    async function handleDeleteQuestion(QuestionId: string) {
+        if(window.confirm('Tem certeza que deseja excluir esta pergunta?')) {
+            await database.ref(`rooms/${roomId}/questions/${QuestionId}`).remove();
+        }
+        
+    }
+
+    return(
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoimg} alt="Letmeask"/>
+                    <div>
+                        <RoomCode code={roomId}/>
+                        <Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
+                    </div>
+                </div>
+            </header>
+
+            <main>
+                <div className="room-title">
+                    <h1>Sala {title}</h1>
+                    {questions.length > 0 && <span>{questions.length} perguntas</span> } 
+                </div>
+                <div className="question-list">
+                {questions.map(question => {
+                    return(
+                        <Question
+                            key={question.id}
+                            content={question.content}
+                            author={question.author}
+                        >
+                            <button
+                                type="button"
+                                onClick={() => handleDeleteQuestion(question.id)}
+                            >
+                                <img src={deleteImg} alt="remover pergunta" />
+                            </button>
+                        </Question>
+                    )
+                })}
+                </div>
+
+            </main>
+        </div>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,9 @@
+import { FormEvent, useState } from 'react';
 import {useHistory} from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
+import toast, { Toaster } from 'react-hot-toast';
+
+import { database } from '../services/firebase';
 
 import illustration from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -9,9 +13,11 @@ import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
 
+
 export function Home() {
     const history = useHistory();
     const {signInWithGoogle, user} = useAuth();
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom() {
         if (!user) {
@@ -21,8 +27,32 @@ export function Home() {
         history.push('/rooms/new');        
     }
 
+    async function handleJoinRoom(event: FormEvent){
+        event.preventDefault();
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            toast.error("Room does not exists.")
+            return;
+        }
+
+        if (roomRef.val().endedAt) {
+            toast.error("Room already closed.")
+            return;
+        }
+
+         history.push(`/rooms/${roomCode}`);
+    }
+
     return(
         <div id="page-auth">
+            <div>
+                <Toaster/>
+            </div>
             <aside>
                 <img src={illustration} alt="imagem de perguntas e respostas" />
                 <strong>Toda pergunta tem uma resposta.</strong>
@@ -38,10 +68,12 @@ export function Home() {
                     <div className="separator">
                         ou entre em uma sala 
                     </div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input 
                         type="text" 
                         placeholder="Digite o código da sala"
+                        onChange={event => setRoomCode(event.target.value)}
+                        value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
