3291162b7f6bf5ff1e37f47f7d81794e0e9d5c50
Pedro Henrique <pedro.semear@gmail.com>
2021-08-11 23:49:17 -0300

    teste deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -57,6 +57,7 @@ export function Home() {
                 <img src={illustration} alt="imagem de perguntas e respostas" />
                 <strong>Toda pergunta tem uma resposta.</strong>
                 <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+                <p>Teste Deploy</p>
             </aside>
             <main>
                 <div className="main-content">
